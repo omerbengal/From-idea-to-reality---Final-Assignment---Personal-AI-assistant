d1320f2b1491fbc30a5d786ee4d2e58b4bdc7c72
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-06-07 21:29:49 +0300

    refactor: change variables' names, extracted code to methods, etc...

--- main.py
+++ main.py
@@ -38,6 +38,10 @@ def setup_credentials():
             creds = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
+    elif creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
     return creds
 
 
@@ -59,15 +63,16 @@ def get_nearest_saturday() -> datetime:
     today = datetime.datetime.now(datetime.timezone.utc)
     # Calculate the number of days to add to reach Saturday (5 - today.weekday())
     # If today is Sunday (weekday() returns 6), we add 6 days to reach the next Saturday
-    days_to_saturday = 5 - today.weekday() if today.weekday() <= 5 else 6
-    return add_days_to_date(today, days_to_saturday)
+    days_from_today_to_nearest_saturday = 5 - \
+        today.weekday() if today.weekday() <= 5 else 6
+    return add_days_to_date(today, days_from_today_to_nearest_saturday)
 
 
 def get_now() -> datetime:
     return datetime.datetime.now(datetime.timezone.utc)
 
 
-def get_events(calendarID: str, time_max: datetime) -> list:
+def get_events_up_to_certain_date(calendarID: str, time_max: datetime) -> list:
     now = get_now().isoformat()
     events_result = (
         CALENDAR_SERVICE.events()
@@ -91,28 +96,49 @@ def get_tasks_lists() -> list:
     return results.get("items", [])
 
 
+def get_tasks_from_list(tasklistID: str) -> list:
+    results = TASKS_SERVICE.tasks().list(tasklist=tasklistID).execute()
+    return results.get("items", [])
+
+
+def example_get_and_print_events():
+    events = get_events_up_to_certain_date(
+        CALENDARS["primary"], get_nearest_saturday())
+
+    if not events:
+        print("No upcoming events found.")
+
+    for event in events:
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        print(start, event["summary"])
+
+
+def example_get_and_print_tasks_from_first_list():
+    tasks_lists = get_tasks_lists()
+
+    for list in tasks_lists:
+        print(f"{list['title']} ({list['id']})")
+
+    print()
+
+    tasks = get_tasks_from_list(tasks_lists[0]["id"])
+
+    for task in tasks:
+        title = task["title"]
+        notes = task.get("notes", "")
+        due = task.get("due", "")
+        status = task.get("status", "")
+        print(f"title: {title}\nnotes: {notes}\ndue: {due}\nstatus: {status}\n")  # nopep8
+
+
 def main():
     try:
         calendar_service_build()
         tasks_service_build()
 
-        events = get_events(CALENDARS["primary"], get_nearest_saturday())
+        example_get_and_print_events()
 
-        # if not events:
-        #     print("No upcoming events found.")
-
-        # for event in events:
-        #     start = event["start"].get("dateTime", event["start"].get("date"))
-        #     print(start, event["summary"])
-
-        lists = get_tasks_lists()
-
-        # if not lists:
-        #     print("No task lists found.")
-        #     return
-
-        # for item in lists:
-        #     print(f"{item['title']} ({item['id']})")
+        example_get_and_print_tasks_from_first_list()
 
     except HttpError as error:
         print(f"An error occurred: {error}")
