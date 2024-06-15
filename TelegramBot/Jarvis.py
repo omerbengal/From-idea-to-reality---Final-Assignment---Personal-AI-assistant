f2a0693d9e5623824e1bea776d5ec2f0db1ba40d
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-06-15 20:06:17 +0300

    chore: re-structure repo + base for Telegram bot

---
+++ TelegramBot/Jarvis.py
@@ -0,0 +1,68 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import os
+import pandas as pd
+
+
+# Constants
+BOT_TOKEN = '<insert_bot_token_here>'
+
+
+# Commands
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'בשביל לבדוק זכאות של חייל מילואים לאירוע מסוים, אנא שלח/י הודעה בפורמט הבא:\n\n'
+        '<שם אירוע>\n<פרט מזהה>\n\n'
+        'דגשים:\n'
+        'שם האירוע נדרש להיות זהה לזה שמופיע ברשימת אירועים (מוזמנים להריץ את הפקודה "/events" לקבלת רשימה של האירועים הקיימים\n)'
+        'פרט מזהה הינו אחד מכמה אפשרויות:\n'
+        '1. תעודת הזהות של משרת המילואים (בפורמט 9 ספרות)\n'
+        '2. מספר הטלפון של משרת המילואים (בפורמט 10 ספרות עם מקף אחרי 3 הספרות הראשונות)\n'
+        '3. מספר הטלפון של בת הזוג של משרת המילואים (בפורמט 10 ספרות עם מקף אחרי 3 הספרות הראשונות)\n\n'
+        'דוגמה:\n'
+        'פסח שוברים\n207827825\n\n'
+        'דוגמה נוספת:\n'
+        'שי ליולדת\n054-8111733\n\n'
+        'לעזרה נוספת - ניתן לפנות לעומר בנגל: 054-8111733'
+    )
+
+
+# Responses
+def handle_response(text: str) -> str:
+    # do some things and return a string
+    pass
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type = update.message.chat.type
+    text = update.message.text
+    # print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    if message_type == 'private':
+        response = handle_response(text)
+        # print('Bot: ', response)
+        await update.message.reply_text(response)
+
+
+# Errors
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # print(f'Update {update} caused error {context.error}')
+    await update.message.reply_text('OOPS! An error occurred. Please try again.')
+
+
+# Main
+if __name__ == '__main__':
+    # print('Starting bot...')
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('help', help_command))
+
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error)
+
+    # print('Polling...')
+    app.run_polling(poll_interval=3)
